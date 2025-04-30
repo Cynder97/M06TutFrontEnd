1573a60af0959f5c7be7e3f607a089881aaa0491
Cynder97 <150056594+Cynder97@users.noreply.github.com>
2025-04-30 14:04:23 -0400

    Added Delete Functionality

---
+++ scripts/delete.js
@@ -0,0 +1,29 @@
+addEventListener("DOMContentLoaded", async function(){
+    document.querySelector("#deletebtn").addEventListener("click", deleteSong);
+    getAllSongs()
+})
+
+async function getAllSongs(){
+    const response = await fetch("https://yummy-numerous-muscle.glitch.me/api/songs")
+    if(response.ok){
+        const songs = await response.json()
+        let html = ""
+        for(let song of songs){
+            html += `<option value="${song._id}">${song.title}</option>`
+        }
+        document.querySelector("#songDropDown").innerHTML = html
+    }
+}
+
+async function deleteSong(){
+    const songID = document.querySelector("#songDropDown option:ckecked").value
+    const response = await fetch("https://yummy-numerous-muscle.glitch.me/api/songs/" + songID, {
+        method: "DELETE"
+    }) 
+    if(response.ok){
+        getAllSongs()
+    }
+    else{
+        document.querySelector("error").innerHTML = "Cannot delete song"
+    }
+}
