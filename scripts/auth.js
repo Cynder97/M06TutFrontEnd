c3d66d0f5de70be096b50e95870d9f82a985a8aa
Cynder97 <150056594+Cynder97@users.noreply.github.com>
2025-05-05 21:30:11 -0400

    fixed typo

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,22 +1,23 @@
-class Auth {
-    constructor() {
-        const auth = localStorage.getItem("auth");
-        this.validateAuth(auth);
+class Auth{
+    constructor(){
+        document.querySelector("body").style.display = "none"
+        const auth = localStorage.getItem("auth")
+        this.validateAuth(auth)
     }
-
-    validateAuth(auth) {
-        if (auth !== "1") {  
-            window.location.href = "login.html";  
-        } else {
-            document.querySelector("body").style.display = "block";
+    validateAuth(auth){
+        if(auth !== 1){
+            window.location.replace("login.html")
+        }
+        else{
+            document.querySelector("body").style.display = "block"
         }
     }
+    
+    logOut(){
+        localStorage.removeItem("auth")
+        localStorage.removeItem("token")
+        localStorage.removeItem("uname")
 
-    logOut() {
-        localStorage.removeItem("auth");
-        localStorage.removeItem("token");
-        localStorage.removeItem("uname");
-
-        window.location.href = "login.html";
+        window.location.replace("login.html")
     }
 }
