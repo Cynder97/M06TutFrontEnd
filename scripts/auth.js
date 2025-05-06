0d98101eba14c19382066087d5d65b467f299888
Cynder97 <150056594+Cynder97@users.noreply.github.com>
2025-05-05 21:25:42 -0400

    Maybe fixed this time

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,23 +1,22 @@
-class Auth{
-    constructor(){
-        document.querySelector("body").style.display = "none"
-        const auth = localStorage.getItem("auth")
-        this.validateAuth(auth)
+class Auth {
+    constructor() {
+        const auth = localStorage.getItem("auth");
+        this.validateAuth(auth);
     }
-    validateAuth(auth){
-        if(auth !== 1){
-            window.location.replace("login.html")
-        }
-        else{
-            document.querySelector("body").style.display = "block"
-        }
-    }
-    
-    logOut(){
-        localStorage.removeItem("auth")
-        localStorage.removeItem("token")
-        localStorage.removeItem("uname")
 
-        window.location.replace("login.html")
+    validateAuth(auth) {
+        if (auth !== "1") {  
+            window.location.href = "login.html";  
+        } else {
+            document.querySelector("body").style.display = "block";
+        }
+    }
+
+    logOut() {
+        localStorage.removeItem("auth");
+        localStorage.removeItem("token");
+        localStorage.removeItem("uname");
+
+        window.location.href = "login.html";
     }
 }
